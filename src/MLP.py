e65bd377d56174af30fa6405b6b1f4f64201ee01
f3nr1r <sotistechno@gmail.com>
2017-11-15 21:00:32 -0500

    Reformat numtofeature and implemented multiclass labeling

--- src/MLP.py
+++ src/MLP.py
@@ -1,77 +1,129 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import src.numtofeature as ntf
 
 
 class MLP:
     def __init__(self, n_input, n_hidden, n_output, n_layer):
+        '''Initializes weight matrices with respect to specified number of neurons
+        for input, hidden and output layer. Also specifies the number of hidden layer
+        of the network. Provides accessibility to the insides of the network with
+        object variables of weights (W), activations(A), errors(E)
+        and partial derivatives of errors (D)
+        n_input : number of input neurons
+        n_hidden : number of neurons in a hidden layer
+        n_output : number of output neurons
+        n_layer : number of layers including hidden and output layers
+                (i.e. 4 hidden => n_layer = 4)'''
+
+        # Set parameters of network
         self.n_input = n_input
         self.n_hidden = n_hidden
         self.n_output = n_output
-        self.n_layer = n_layer + 1
+        self.n_layer = n_layer + 2
         self.W = [0.0] * self.n_layer
         self.A = [0.0] * self.n_layer
         self.E = [0.0] * self.n_layer
-        self.deltas = [0.0] * self.n_layer
+        self.D = [0.0] * self.n_layer
+        self.B = [0.0] * self.n_layer
 
-        # Initialize weights of layers according to depth of network
+        # Initialize weights and biases of layers according to depth of network
         if self.n_layer < 1:
             raise ValueError('n_layer should be greater or equal to 2')
-        if self.n_layer == 1:
+        if self.n_layer == 2:
             self.W[0] = np.random.random((self.n_input, self.n_output))
+            self.B[0] = np.zeros((1, self.n_output))
         else:
             self.W[0] = np.random.random((self.n_input, self.n_hidden))
+            self.B[0] = np.zeros((1, self.n_hidden))
             self.W[self.n_layer - 1] = np.random.random((self.n_hidden, self.n_output))
-            for depth in range(1, self.n_layer - 2):
+            self.B[self.n_layer - 1] = np.zeros((1, self.n_hidden))
+            for depth in range(1, self.n_layer - 1):
                 self.W[depth] = np.random.random((self.n_hidden, self.n_hidden))
+                self.B[depth] = np.zeros((1, self.n_hidden))
 
-
-    # Computes sigmoid of a value
     def _sigmoid(self, x):
+        '''Computes sigmoid of a value'''
         return 1 / (1 + np.exp(-x))
 
-    # Computes derivative of sigmoid function
     def _dsigmoid(self, y):
+        '''Computes derivative of sigmoid function'''
         return y * (1.0 - y)
 
+    def _softmax(self, x):
+        """Compute softmax values for each sets of scores in x."""
+        return np.exp(x) / np.sum(np.exp(x), axis=0)
+
     def feedforward(self, inputs):
-        # FEED-FORWARD
-        for depth in range(self.n_layer):
+        '''Fills the activation matrix of the neurons
+        and outputs the results at the end for one epoch'''
+        for depth in range(self.n_layer - 1):
             # Compute activation of first hidden layer
             if depth == 0:
                 self.A[depth] = self._sigmoid(np.dot(inputs, self.W[depth]))
+            # Activation of every other layer
             else:
                 self.A[depth] = self._sigmoid(np.dot(self.A[depth - 1], self.W[depth]))
+        self.A[-1] = self._sigmoid(np.dot(self.A[-2], self.W[-1]))
+
         return self.A[-1]
-    # Train the weights of the neural network
-    def fit(self, inputs, targets, learning_rate=1.00, n_epochs=200000):
+
+    def fit(self, inputs, targets, learning_rate=0.01, n_epochs=200000):
+        '''Train the weights of a custom network by computing activations from feedforward
+        and then backpropagating the errors after one epoch. Uses simple error as loss function.'''
+        ERROR = []
+
         for j in range(n_epochs):
             # Apply feed forward for epoch
-            self.feedforward(inputs)
-
-            # BACKPROPAGATION
-            # Squared euclidean distance loss function
             for depth in reversed(range(self.n_layer)):
+                for idx, batch in enumerate(inputs):
+                    self.feedforward(batch)
+                    # BACKPROPAGATION
+                    # Squared euclidean distance cost function
+                    # Compute the error and derivative error of output layers' neurons
+                    if depth == self.n_layer - 1:
+                        self.E[depth] = targets[idx] - self.A[depth] # Derivative of the squared euclidean distance
+                        self.D[depth] = np.multiply(self.E[depth], self._dsigmoid(self.A[depth]))
+                    # Compute the error and derivative error of hidden layers' neurons
+                    else:
+                        self.E[depth] = np.dot(self.D[depth + 1], self.W[depth + 1].T)
+                        self.D[depth] = self.E[depth] * self._dsigmoid(self.A[depth])
 
-                # Compute the contribution of neuron to error of an latter neuron
-                if depth == self.n_layer - 1:
-                    self.E[depth] = targets - self.A[depth]
-                    self.deltas[depth] = self.E[depth] * self._dsigmoid(self.A[depth])
-                else:
-                    self.E[depth] = np.dot(self.deltas[depth + 1], self.W[depth + 1].T)
-                    self.deltas[depth] = self.E[depth] * self._dsigmoid(self.A[depth])
+                    # Update weights based on contribution of neuron to error
+                    if depth == 0:
+                        self.W[depth] += np.dot(batch.T, self.D[depth]) * learning_rate
+                    else:
+                        self.W[depth] += np.dot(self.A[depth - 1].T, self.D[depth]) * learning_rate
 
-                # Update weights based on contribution of neuron to error
-                if depth == 0:
-                    self.W[depth] += np.dot(inputs.T, self.deltas[depth]) * learning_rate
-                else:
-                    self.W[depth] += np.dot(self.A[depth - 1].T, self.deltas[depth]) * learning_rate
-            if (j % 10000) == 0:
-                print("Error:" + str(np.mean(np.abs(self.E[self.n_layer-1]))))
+                    if (j % 1000) == 0:
+                        print("Error:" + str(np.mean(np.abs(self.E[self.n_layer - 1]))))
+                        ERROR.append(np.mean(np.abs(self.E[-1])))
+
+                        plt.xlabel('Epochs (n)')
+                        plt.ylabel('Cost function')
+                        plt.plot(range(0, len(ERROR)), ERROR)
+                        plt.pause(0.000001)
+        plt.show()
+
     def predict(self, i):
+        '''Returns the output of the network given an input matching input neurons'''
         return self.feedforward(i)
 
+
 if __name__ == "__main__":
-    mlp = MLP(2, 3, 1, 1)
-    inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    targets = np.array([[0], [1], [1], [0]])
-    mlp.fit(inputs, targets)
+    mlp = MLP(2, 3, 4, 1)
+    # inputs = np.array([[[0, 0], [0, 1], [1, 0], [1, 1]],
+    #                    [[0, 0], [0, 1], [1, 0], [1, 1]],
+    #                    [[0, 0], [0, 1], [1, 0], [1, 1]],
+    #                    [[0, 0], [0, 1], [1, 0], [1, 1]]])
+    # targets = np.array([[[0, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0]],
+    #                     [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 0, 0]],
+    #                     [[0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0]],
+    #                     [[0, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]])
+
+    answer_key, dgtLst = ntf.read_digit_file('data/smalltrain.csv', 2, 2)
+    mlp.fit(inputs, targets, learning_rate=0.1)
+    print(str(mlp.predict([0, 0])))
     print(str(mlp.predict([0, 1])))
+    print(str(mlp.predict([1, 0])))
+    print(str(mlp.predict([1, 1])))
--- src/numtofeature.py
+++ src/numtofeature.py
@@ -5,88 +5,90 @@ This file handles compressing, and manipulating song information to
 be entered into our Artificial Neural Network
 '''
 
-
 import sys, os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def wait():
     programPause = raw_input("Press the <ENTER> key to continue...")
 
+
 #########   Explanation  #######################
 # reduces size of hand-drawn image vector
-#########   Input    #######################
+#########   Input   cell #######################
 # numimage: 1d numpy vector of grayscale numbers of handrawn digit
 # rowcells: number of cells in y direction to compress down to
 # clmcells: number of cells in x direction to compress down to
 #########   Output  #######################
-#will output 1D numpy array of length rowcells x clmcells
+# will output 1D numpy array of length rowcells x clmcells
 def compress(numimage, rowcells, clmcells):
-	'''
-	this function will input
-	'''
-	istep = 28/rowcells
-	jstep = 28/clmcells
+    '''
+    this function will input
+    '''
+    istep = 28 / rowcells
+    jstep = 28 / clmcells
 
-	ftrvec = np.empty([rowcells*clmcells,])
-	for i in range(rowcells):
-		for j in range(clmcells):
-			strt = i*istep+j*28*jstep
-			end = strt+istep
-			cell = numimage[strt:end]
-			for k in range(1, jstep):
-				strt = i*istep+j*28*jstep+k*28
-				end = strt+istep
+    ftrvec = np.empty([rowcells * clmcells, ])
+    for i in range(rowcells):
+        for j in range(clmcells):
+            strt = i * istep + j * 28 * jstep
+            end = strt + istep
+            cell = numimage[strt:end]
+            for k in range(1, jstep):
+                strt = i * istep + j * 28 * jstep + k * 28
+                end = strt + istep
 
-				tmp=numimage[strt:end]
-				cell = np.concatenate((cell, tmp), axis=0)
+                tmp = numimage[strt:end]
+                cell = np.concatenate((cell, tmp), axis=0)
 
-			cellcolor = np.mean(cell)
-			
-			ftrvec[i*rowcells+j]=cellcolor
-	# debug
-	# print(ftrvec)
-	return ftrvec
+            cellcolor = np.mean(cell)
+
+            ftrvec[i * rowcells + j] = cellcolor
+    # debug
+    # print(ftrvec)
+    return ftrvec
 
 
 def print_cmprsd(ftrvec, rowcells, clmcells):
-  istep = (28/rowcells)
-  jstep = (28/clmcells)
+    istep = (28 / rowcells)
+    jstep = (28 / clmcells)
 
-  for j in range(clmcells):
-    rowlst=[]
-    for i in range(rowcells):
-    	cellclr=str(ftrvec[i*rowcells+j])
-    	rowlst.append(cellclr)
-    print (rowlst)
+    for j in range(clmcells):
+        rowlst = []
+        for i in range(rowcells):
+            cellclr = str(ftrvec[i * rowcells + j])
+            rowlst.append(cellclr)
+        print (rowlst)
+
 
 #########   Explanation  #######################
 # reads file containing many hand drawn image files
 #########   Input    #######################
-# digFile:  file to read digits from
+# digFile:  file to read digits from16
 # rowcells: number of cells in y direction to compress down to
 # clmcells: number of cells in x direction to compress down to
 #########   Output  #######################
-#will output 1D numpy array of length rowcells x clmcells
+# will output 1D numpy array of length rowcells x clmcells
 def read_digit_file(digFile, rowcells, clmcells):
+    answerKey = []  # this will hold the actual value of the digit entry
+    digitLst = []  # will hold 1D numpy vecs of the data
 
-	answerKey = []	#this will hold the actual value of the digit entry
-	digitLst = []	#will hold 1D numpy vecs of the data
+    trainDF = pd.read_csv(digFile, header=0, index_col=False)
 
-	trainDF = pd.read_csv(digFile, header=0, index_col=False)
+    answerKey = trainDF['label'].tolist()
 
-	answerKey = trainDF['label'].tolist()
+    # Delete the label from panda dataframe,
+    trainDF.drop('label', axis=1)
 
-	# Delete the label from panda dataframe,
-	trainDF.drop('label', axis=1)
+    digitLst = trainDF.values.tolist()
 
-	digitLst = trainDF.values.tolist()
+    for i in range(len(digitLst)):
+        digitLst[i] = compress(digitLst[i], rowcells, clmcells)
 
-	for i in range(len(digitLst)):
-		digitLst[i] = compress(digitLst[i],rowcells, clmcells)
+    return answerKey, digitLst
 
-	return answerKey, digitLst
 
 #########   Explanation  #######################
 # agglomerates several songs into a batch
@@ -95,29 +97,29 @@ def read_digit_file(digFile, rowcells, clmcells):
 # size: 	number of hand drawn digit image files to include in batch
 # step: 	the batch number to produce, makes sure covers whole data
 #########   Output  #######################
-#will output numpy array of [size x digit features]
+# will output numpy array of [size x digit features]
 
 def batchify(digitLst, size, step):
+    numsteps = int(len(digitLst) / size) - 1
 
-	numsteps = int(len(digitLst)/size)-1
+    # Debug
+    # print(step*size + size , len(digitLst) )
 
-	# Debug
-	# print(step*size + size , len(digitLst) )
+    if (step * size + size > len(digitLst)):
+        print("***ERRROR : Step causes too small of a batch")
+        return 0
+    else:
+        batch = np.array(digitLst[step * size])
+        for i in range(1, size):
+            tmp = digitLst[step * size + i]
+            # 1d batch
+            # batch = np.concatenate((batch, digitLst[step*size+i]), axis=0)
+            # 2d batch
+            batch = np.vstack((batch, tmp))
+        # debug
+        # print("Batch return", type(batch), type(batch[0]))
+        return batch
 
-	if(step*size + size > len(digitLst)):
-		print("***ERRROR : Step causes too small of a batch")
-		return 0
-	else:
-		batch = np.array(digitLst[step*size])
-		for i in range(1, size):
-			tmp =  digitLst[step*size+i]
-			#1d batch
-			# batch = np.concatenate((batch, digitLst[step*size+i]), axis=0)
-			#2d batch
-			batch = np.vstack((batch, tmp))
-		# debug
-		# print("Batch return", type(batch), type(batch[0]))
-		return batch
 
 ###############################
 ### Main Program ##############
@@ -147,9 +149,9 @@ print(len(answerKey), len(digitLst), digitLst[0].shape)
 # wait()
 
 print("************Batch***************")
-for step in range(int(len(digitLst)/batchsize)):
-	batch = batchify(digitLst, batchsize, step)
+for step in range(int(len(digitLst) / batchsize)):
+    batch = batchify(digitLst, batchsize, step)
 
-	# debug
-	# print(batch)
-	# print(batch.shape)
+# debug
+# print(batch)
+# print(batch.shape)
