849e7107f7a73e6a282b551cf7c1cace501511de
jmw992 <jmw992@gmail.com>
2017-11-16 17:31:58 -0500

    Add files via upload
    Changes to numfeature, included hotvec functionality in reading functions
    -added reading function to create training subset and testing subset from a training file
    -created seperate reading function for reading testing file which won't have answer label colum

--- src/numtofeature.py
+++ src/numtofeature.py
@@ -13,6 +13,7 @@ import pandas as pd
 
 def wait():
     programPause = raw_input("Press the <ENTER> key to continue...")
+    #programPause = input("Press the <ENTER> key to continue...")
 
 
 #########   Explanation  #######################
@@ -27,8 +28,8 @@ def compress(numimage, rowcells, clmcells):
     '''
     this function will input
     '''
-    istep = 28 / rowcells
-    jstep = 28 / clmcells
+    istep = int(28 / rowcells)
+    jstep = int(28 / clmcells)
 
     ftrvec = np.empty([rowcells * clmcells, ])
     for i in range(rowcells):
@@ -37,8 +38,8 @@ def compress(numimage, rowcells, clmcells):
             end = strt + istep
             cell = numimage[strt:end]
             for k in range(1, jstep):
-                strt = i * istep + j * 28 * jstep + k * 28
-                end = strt + istep
+                strt = int(i * istep + j * 28 * jstep + k * 28)
+                end = int(strt + istep)
 
                 tmp = numimage[strt:end]
                 cell = np.concatenate((cell, tmp), axis=0)
@@ -50,11 +51,8 @@ def compress(numimage, rowcells, clmcells):
     # print(ftrvec)
     return ftrvec
 
-
+#function to print compressed 1d pixel in 2d format
 def print_cmprsd(ftrvec, rowcells, clmcells):
-    istep = (28 / rowcells)
-    jstep = (28 / clmcells)
-
     for j in range(clmcells):
         rowlst = []
         for i in range(rowcells):
@@ -62,6 +60,24 @@ def print_cmprsd(ftrvec, rowcells, clmcells):
             rowlst.append(cellclr)
         print (rowlst)
 
+#########   Explanation  #######################
+# converts answerkey of digits 0-9 into binary version
+#of length ten with position marking answerkey digit
+def one_hot_vector(answerKey):
+    '''Performs encoding for class labels of training set'''
+    onehotvectors = []
+    for key in answerKey:
+        empty_vec = np.zeros((1, 10), dtype=np.int)
+        empty_vec[0][key] = 1
+        onehotvectors.append(empty_vec)
+    return onehotvectors
+
+#########   Explanation  #######################
+# converts digit 0-9 answer to binary len 10 vector
+def digtoHotVec(digit):
+    empty_vec = np.zeros((1, 10), dtype=np.int)
+    empty_vec[0][digit] = 1
+    return empty_vec
 
 #########   Explanation  #######################
 # reads file containing many hand drawn image files
@@ -70,8 +86,10 @@ def print_cmprsd(ftrvec, rowcells, clmcells):
 # rowcells: number of cells in y direction to compress down to
 # clmcells: number of cells in x direction to compress down to
 #########   Output  #######################
-# will output 1D numpy array of length rowcells x clmcells
-def read_digit_file(digFile, rowcells, clmcells):
+# answerKey: 1D numpy array of length rowcells x clmcells for answerKey
+# digitLst:  list of 1D numpy arrays of length rowcells x clmcells, one for 
+#            each digit
+def read_train(digFile, rowcells, clmcells):
     answerKey = []  # this will hold the actual value of the digit entry
     digitLst = []  # will hold 1D numpy vecs of the data
 
@@ -82,20 +100,83 @@ def read_digit_file(digFile, rowcells, clmcells):
     # Delete the label from panda dataframe,
     trainDF.drop('label', axis=1)
 
-    # Pop first column of labels
-    digitLst = np.array(trainDF.values.tolist())[:,1:]
+    digitLst = trainDF.values.tolist()
 
-    if rowcells*clmcells != 784:
-        for i in range(len(digitLst)):
-            digitLst[i] = compress(digitLst[i], rowcells, clmcells)
+    for i in range(len(digitLst)):
+        answerKey[i] = digtoHotVec(answerKey[i])
+        digitLst[i] = compress(digitLst[i], rowcells, clmcells)
 
-    return answerKey, normalize(digitLst)
+    return answerKey, digitLst
 
+#########   Explanation  #######################
+# same as read_train, but no answerkey required
+def read_test(testFile, rowcells, clmcells):
+    digitLst = []  # will hold 1D numpy vecs of the data
+
+    testDF = pd.read_csv(testFile, header=0, index_col=False)
+
+    digitLst = testDF.values.tolist()
+
+    for i in range(len(digitLst)):
+        digitLst[i] = compress(digitLst[i], rowcells, clmcells)
+
+    return digitLst
+
+#########   Explanation  #######################
+# same as read_train, but specify training size and
+# size of testing set, gives answer key to testing to see
+# if learning is effective
+def read_trn_partial(digFile, rowcells, clmcells, numTrn, numTst):
+    answerKey=[]
+    digitLst=[]
+    
+    trnKey = []  # this will hold the actual value of the digit entry
+    trnLst = []  # will hold 1D numpy vecs of the data
+    
+    tstKey = []  # this will hold the actual value of the digit entry
+    tstLst = []  # will hold 1D numpy vecs of the data
+    
+    trainDf = pd.read_csv(digFile, header=0, index_col=False)
+    
+    '''
+    #This function takes a long time to run so decomment these lines if you
+    #will be running the same training and testing set over and over
+    droprows = range(numTrn+numTst+1, len(trainDf))
+    smallDf= trainDf.drop(trainDf.index[droprows])
+    smallDf.to_csv("1000trn100tst.csv")
+    wait()
+    '''
+    
+    answerKey = trainDf['label'].tolist()
+
+    # Delete the label from panda dataframe,
+    trainDf.drop('label', axis=1)
+
+    digitLst = trainDf.values.tolist()
+
+    for i in range(numTrn):
+        trnLst.append(compress(digitLst[i], rowcells, clmcells))
+        trnKey.append(digtoHotVec(answerKey[i]))
+    
+    if(numTrn+numTst>len(answerKey)):
+        print("Error: numTest + numTrain must be less than number of digits in digit File")
+        for i in range(numTrn, len(answerKey)):
+            tstLst.append(compress(digitLst[i], rowcells, clmcells))
+            tstKey.append(digtoHotVec(answerKey[i]))          
+    else:
+        for i in range(numTrn, numTrn+numTst):
+            tstLst.append(compress(digitLst[i], rowcells, clmcells))
+            tstKey.append(digtoHotVec(answerKey[i]))          
+
+    return trnLst, trnKey, tstLst, tstKey
 
 #########   Explanation  #######################
 # agglomerates several songs into a batch
 #########   Input    #######################
 # digitList:	1-D compressed array of hand drawn digit image data
+# target_vector: list of 1D vector, 1 per digit in batch.  Each 1d vec,
+#                   is of size 10, holding binary value of whether position
+#                   is the true digit according to answer key
 # size: 	number of hand drawn digit image files to include in batch
 # step: 	the batch number to produce, makes sure covers whole data
 #########   Output  #######################
@@ -128,36 +209,32 @@ def batchify(digitLst, target_vector, size, step):
         else:
             return target_b, batch
 
-def one_hot_vector(answerKey):
-    '''Performs encoding for class labels of training set'''
-    onehotvectors = []
-    for key in answerKey:
-        empty_vec = np.zeros((1, 10))
-        empty_vec[0][key] = 1
-        onehotvectors.append(empty_vec)
-    return onehotvectors
+
 
 def normalize(digitLst):
     '''Normalize input between 0 and 1'''
     return digitLst/256
 
+
 if __name__ == "__main__":
     ###############################
     ### Main Program ##############
     ###############################
-    trainFile = "smalltrain.csv"
+    trainFile = "Data/1000trn100tst.csv"
 
-    rowcells = 14
-    clmcells = 14
+    rowcells = 4
+    clmcells = 4
+    
+    numTrn = 1000
+    numTst = 100
 
     batchsize = 3
 
-    answerKey, digitLst = read_digit_file(trainFile, rowcells, clmcells)
-
+    #answerKey, digitLst = read_train(trainFile, rowcells, clmcells)
+    #targets = one_hot_vector(answerKey)
     # debug
-    print("answerkey developed")
-    print("read trn", type(answerKey), type(digitLst), type(digitLst[0]))
-    print(len(answerKey), len(digitLst), digitLst[0].shape)
+    #print("read trn", type(answerKey), type(answerKey[0]), type(digitLst), type(digitLst[0]))
+    #print(len(answerKey), answerKey[0], len(digitLst), digitLst[0].shape)
 
     # wait()
 
@@ -168,11 +245,13 @@ if __name__ == "__main__":
     # print("************Print Cmprsd**********")
 
     # wait()
-
+    print("************Read Train Partial*********")
+    trnLst, trnKey, tstLst, tstKey = read_trn_partial(trainFile, rowcells, clmcells, numTrn, numTst)
+    
     print("************Batch***************")
     for step in range(int(len(digitLst) / batchsize)):
-        batch = batchify(digitLst, batchsize, step)
+        answer_b, batch = batchify(digitLst, answerKey, batchsize, step)
 
     # debug
-    # print(batch)
-    # print(batch.shape)
+    print(batch)
+    print(batch.shape)
