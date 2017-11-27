6f5e07a03af8bfca7d5d67cdacff138186afb245
jmw992 <jmw992@gmail.com>
2017-11-27 14:29:06 -0500

    Add files via upload
    Added dropout method, turns off a portion of hidden neurons activation, updated comments in mlp  __init__ .  Changed fit so it outputs training and testing accuracy outputs to csv

--- src/MLP_v4.py
+++ src/MLP_v4.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # import src.numtofeature as ntf
 import numtofeature as ntf
 import pandas as pd
-
+from random import sample
 
 def wait():
     # programPause = raw_input("Press the <ENTER> key to continue...")
@@ -12,18 +12,33 @@ def wait():
 
 
 class MLP:
-    def __init__(self, lyrNodes, lrnRate=0.1, regParam=1, batch_size=10,
-                 hiddenActv='sigmoid', outputActv='softmax', costFnc='sqrdEuc', n_epochs=200):
+    def __init__(self, lyrNodes, lrnRate=0.1, batch_size=10,  n_epochs=200, 
+                  hiddenActv='sigmoid', outputActv='softmax', costFnc='sqrdEuc', 
+                   regMethod ='dropout', regParam=0, dropPrtn=0.2, momentum=1):
+        
         '''Initializes weight matrices with respect to specified number of neurons
         for input, hidden and output layer. Also specifies the number of hidden layer
         of the network. Provides accessibility to the insides of the network with
         object variables of weights (W), activations(A), errors(E)
         and partial derivatives of errors (D)
-        n_input : number of input neurons
-        n_hidden : number of neurons in a hidden layer
-        n_output : number of output neurons
-        n_layer : number of layers including hidden and output layers
-                (i.e. 4 hidden => n_layer = 4)'''
+        lyrNodes: list of ints specifying number of Nodes(neurons), in each layer
+                    order of [layer input, hidden0,hidden1...hiddenN,Output]
+        lrnRate = learning rate, alpha, for weight updates in backprops
+        batch_size = number of training examples into each batch
+        
+        n_epochs = number of training iterations to use with backprop
+        costFunc : 'xentropy' or 'sqrdEuc'    
+        outputActv : 'softmax' or 'softplus' or 'sigmoid'
+        hiddenActv : 'softmax' or 'softplus' or 'sigmoid'
+        mlp.regPthd : 'weightedDst' or 'dropout'
+        regParam : regularization parameter used for weighted distance, if using dropout
+                    set to zero can be any int, but higher number may degrade accuracy
+        dropPrtn : portion of neurons in hidden layer to dropout for regularization. 
+                    Not used in weightedDist, must be a decimal recomended .1=.5 
+        momentum : momentum factor to change learning rate in back prop
+                    based on magnitude of last error.  If don't want to use set
+                    to zero
+        '''
 
         # Set parameters of network
         self.n_input = lyrNodes[0]
@@ -35,8 +50,9 @@ class MLP:
         self.costFnc = costFnc # Cost function used for error and gradient descent
         self.n_epochs = n_epochs # Number of epochs for learning
         self.alpha = lrnRate  # learning rate parameter
-        self.lmda = regParam  # regularization parameter
-        self.mntmRate = 1.0 # Momentum factor
+        
+        
+        self.mntmRate = momentum # Momentum factor
         self.cost = 0 # initialize cost value
         self.W = [0.0] * self.n_layer  # Weight of neurons in each layers
         self.A = [0.0] * self.n_layer  # Output of neurons in each layers
@@ -44,15 +60,20 @@ class MLP:
         self.dW = [0.0] * self.n_layer  # Rate of change of cost function for each neurons
         self.update = [0.0] * self.n_layer # Store update vector for next update with momentum
         self.B = [0.0] * self.n_layer  # Bias term of each neuron
-
+        self.regMthd = regMethod    #'dropout' or 'weightDist'
+        self.drpLst = []   #lst of neurons to set to dropout from FF and Backprop
+        self.drpPrtn = dropPrtn  #what portion of hidden neurons in each hidden layer to turn off
+        self.lmda = regParam  # regularization parameter
+        
         # Initialize weights and biases of layers according to depth of network
         if self.n_layer < 1:
             raise ValueError('n_layer should be greater or equal to 1')
-
+        
+        #if there is no hidden layer
         if self.n_layer == 1:
             self.W[0] = np.random.normal(scale=0.1, size=(self.n_input, self.n_output))
             self.B[0] = np.random.normal(scale=0.1, size=(1, self.n_output))
-            self.update[0] = np.zeros((self.n_input, self.n_output))
+            self.update[0] = np.zeros((self.n_input, self.n_output))   
         else:
             for depth in range(0, len(lyrNodes) - 1):
                 self.W[depth] = np.random.normal(scale=0.1, size=(lyrNodes[depth], lyrNodes[depth + 1]))
@@ -79,11 +100,6 @@ class MLP:
             pretty[0][maxpos] = 1
 
             if (maxpos != np.argmax(tstKey[i])):
-                '''
-                print(tstKey[i])
-                print(pretty)
-                print(np.argmax(tstKey[i])," ", maxpos)
-                '''
                 inaccurate += 1.0
         accuracy = 100 - 100 * float(inaccurate / len(tstLst))
         return accuracy
@@ -106,7 +122,6 @@ class MLP:
     def softplusElement(self, x):
         # approximate of ReLU that you can take a derivative of
         # https://www.quora.com/What-is-special-about-rectifier-neural-units-used-in-NN-learning
-        # print(x)
         tmp = 1 + np.exp(x)
         y = np.log(tmp)
         return y
@@ -114,7 +129,6 @@ class MLP:
     # jmw
     # elementwise version of derivative of ReLU equation
     def dsoftplusElement(self, x):
-        # print("dsplus elm", x)
         y = 1 / (1 + np.exp(-x))
         return y
 
@@ -170,17 +184,33 @@ class MLP:
     def feedforward(self, inputs):
         '''Fills the activation matrix of the neurons
         and outputs the results at the end for one epoch with chosen activation function'''
+        self.dropMsk = [0]*self.n_layer
         for depth in range(self.n_layer):
             # Compute activation of first hidden layer
             if depth == 0:
                 self.A[depth] = self._activation(np.dot(inputs, self.W[depth]) + self.B[depth], self.hiddenActv)
+                if (self.regMthd=='dropout'):
+                    #randomly select positions which will be turned off
+                    numNrns = len(self.A[depth][0])
+                    trnoff = sample(range(0,numNrns), int(round(self.drpPrtn*numNrns, 0)))
+                    for x in trnoff:
+                        for row in self.A[depth]:
+                            row[x]=0
+
             # Activation of every other layer
             else:
                 self.A[depth] = self._activation(np.dot(self.A[depth - 1], self.W[depth]) + self.B[depth],
                                                  self.hiddenActv)
-
+                if (self.regMthd=='dropout'):
+                    #randomly select positions which will be turned off
+                    numNrns = len(self.A[depth][0])
+                    trnoff = sample(range(0,numNrns), int(round(self.drpPrtn*numNrns, 0)))
+                    for x in trnoff:
+                        for row in self.A[depth]:
+                            row[x]=0
+                
+        #handling output layer
         self.A[depth] = self._activation(np.dot(self.A[depth - 1], self.W[depth]) + self.B[depth], self.outputActv)
-        # print("activation", self.A[depth])
         # return output layer
         return self.A[depth]
 
@@ -194,10 +224,11 @@ class MLP:
                 if self.costFnc == 'sqrdEuc':
                     self.cost = np.sum(self.cost)
                     self.dW[depth] = np.multiply(self.dC[depth], self._activation(self.A[depth], self.outputActv,
-                                                                                  derivative=True))  # Haddamard product
+                                                                                      derivative=True))  # Haddamard product
                 elif self.costFnc == 'xentropy':
                     self.cost = -np.mean(self.cost)
                     self.dW[depth] = self.dC[depth]
+                    
             # Compute the error and derivative error of hidden layers' neurons
             else:
                 self.dC[depth] = np.dot(self.dW[depth + 1], self.W[depth + 1].T)
@@ -211,15 +242,18 @@ class MLP:
             # if input layer
             if (depth == 0):
                 self.update[depth] = (np.dot(inputs.T, self.dW[depth])) * self.alpha + self.mntmRate*self.update[depth]
+                
                 regularized = self.W[depth] * (1 - self.alpha * self.lmda / len(inputs))
                 self.W[depth] = regularized - self.update[depth]
 
             if (depth != 0):
                 self.update[depth] = (np.dot(self.A[depth - 1].T, self.dW[depth])) * self.alpha + self.mntmRate*self.update[depth]
+                
                 regularized = self.W[depth] * (1 - self.alpha * self.lmda / len(inputs))
                 self.W[depth] = regularized - self.update[depth]
 
             # then update bias
+            
             self.B[depth] -= np.mean(self.dW[depth], axis=0) * self.alpha
 
     def fit(self, inputs, targets, tstData, tstKey):
@@ -246,7 +280,8 @@ class MLP:
 
             trnErr.append(self.testAccuracy(inputs, targets))
             tstErr.append(self.testAccuracy(tstData, tstKey))
-
+            
+            '''
             # print("Trn Error,", trnErr,"Tst Error,", tstErr)
             if (j % 5 == 0):
                 # printlst = []
@@ -271,16 +306,17 @@ class MLP:
                 plt.plot(range(0, len(trnErr)), trnErr, 'bo')
                 plt.plot(range(0, len(tstErr)), tstErr, 'r--')
                 plt.pause(0.000001)
-
-        plt.show()
+        '''
+        return trnErr, tstErr
+        #plt.show()
 
 
 if __name__ == "__main__":
     # n_input, n_hidden, n_output, n_layer (optional specify learning rate and reg param)
-    mlp = MLP([784, 100, 10])
+    mlp = MLP([784, 135, 10])
     mlp.hiddenActv = 'softplus'
-    mlp.batch_size = 100
-    mlp.n_epochs = 1000
+    mlp.batch_size = 17
+    mlp.n_epochs = 800
     mlp.alpha = 0.01
     mlp.mntmRate = 0.0
     mlp.outputActv = 'softmax'
@@ -303,14 +339,27 @@ if __name__ == "__main__":
     print('Number of hidden layers = ' + str(mlp.n_layer - 1))
     print('Hidden layers activation function = ' + str(mlp.hiddenActv))
     print('Output layer activation function = ' + str(mlp.outputActv))
+    print('Regularization Method = ' + mlp.regMthd)
     print('Batch size = ' + str(mlp.batch_size))
     print('Cost function = ' + str(mlp.costFnc))
     print('Training epochs = ' + str(mlp.n_epochs))
-    mlp.fit(trnLst, trnKey, tstLst, tstKey)
+    
+    trnAcc, tstAcc = mlp.fit(trnLst, trnKey, tstLst, tstKey)
+    
+    prntlst=[]
+    prntlst.append(trnAcc)
+    prntlst.append(tstAcc)
+    prntlst = np.array(prntlst)
+    
+    printDF = pd.DataFrame(prntlst)
+    csvname = "DropTest1.csv"
+    printDF.to_csv(csvname)
     print ('==========Training done')
 
     # mlp.testAccuracy(trnLst, trnKey)
     # mlp.testAccuracy(tstLst, tstKey)
+
+    
 =======
 import numpy as np
 import matplotlib.pyplot as plt
@@ -522,7 +571,7 @@ class MLP:
                 lrnupdate = (np.dot(inputs.T, self.dW[depth])) * self.alpha
                 regularized = self.W[depth] * (1 - self.alpha * self.lmda / len(inputs))
                 self.W[depth] = regularized - lrnupdate
-
+            #if hidden layer
             if (depth != 0):
                 lrnupdate = (np.dot(self.A[depth - 1].T, self.dW[depth])) * self.alpha
                 regularized = self.W[depth] * (1 - self.alpha * self.lmda / len(inputs))
@@ -607,6 +656,8 @@ if __name__ == "__main__":
     mlp.alpha = 0.01
     mlp.outputActv = 'softmax'
     mlp.costFnc = 'xentropy'
+    mlp.regMthd = 'dropout'
+    mlp.drpPrtn = 0.3
     # Test XOR, AND, OR and NOR inputs and targets
     # inputs = np.array([[[0, 0], [0, 1], [1, 0], [1, 1]],
     #                    [[0, 0], [0, 1], [1, 0], [1, 1]],
